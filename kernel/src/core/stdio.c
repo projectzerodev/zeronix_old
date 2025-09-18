5e65f5cf2d576960e60e2d4bf74acf1836cdf674
Viktor Popp <viktorpopp.dev@gmail.com>
2025-09-18 14:43:26 +0200

    `kprintf` works using `nanoprintf`

--- kernel/src/core/stdio.c
+++ kernel/src/core/stdio.c
@@ -1,14 +1,37 @@
 #include "stdio.h"
 #include "../graphics/terminal.h"
-#include <printf.h>
+#include <stdarg.h>
+
+#define NANOPRINTF_USE_FIELD_WIDTH_FORMAT_SPECIFIERS     1
+#define NANOPRINTF_USE_PRECISION_FORMAT_SPECIFIERS       1
+#define NANOPRINTF_USE_FLOAT_FORMAT_SPECIFIERS           0
+#define NANOPRINTF_USE_SMALL_FORMAT_SPECIFIERS           1
+#define NANOPRINTF_USE_LARGE_FORMAT_SPECIFIERS           1
+#define NANOPRINTF_USE_BINARY_FORMAT_SPECIFIERS          1
+#define NANOPRINTF_USE_WRITEBACK_FORMAT_SPECIFIERS       0
+#define NANOPRINTF_SNPRINTF_SAFE_TRIM_STRING_ON_OVERFLOW 1
+
+#define NANOPRINTF_IMPLEMENTATION
+#include <nanoprintf.h>
 
 void _putchar(char character) {
     _term_write(&character, sizeof(char));
 }
 
-void kprintf(const char *format, ...) {
+int kprintf(const char *format, ...) {
+    (void)format;
+    char buffer[1024];
     va_list args;
+
     va_start(args);
-    printf_(format, args);
+    int length = npf_vsnprintf(buffer, sizeof(buffer), format, args);
     va_end(args);
+
+    if (length < 0 || length >= (int)sizeof(buffer)) {
+        return -1;
+    }
+
+    _term_write(buffer, length);
+
+    return length;
 }
--- kernel/src/core/stdio.h
+++ kernel/src/core/stdio.h
@@ -1,3 +1,3 @@
 #pragma once
 
-void kprintf(const char *format, ...);
+int kprintf(const char *format, ...);
