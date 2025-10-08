6a9f6d1207012e5be2a9be5170ba9ef2e8da1f51
Viktor Popp <viktorpopp.dev@gmail.com>
2025-10-08 19:21:20 +0200

    Added spinlocks

---
+++ kernel/src/lib/spinlock.c
@@ -0,0 +1,19 @@
+#include "spinlock.h"
+
+void spinlock_init(spinlock_t *lock)
+{
+    atomic_flag_clear(lock);
+}
+
+void spinlock_acquire(spinlock_t *lock)
+{
+    while (atomic_flag_test_and_set(lock))
+    {
+        __asm__ __volatile__("pause");
+    }
+}
+
+void spinlock_release(spinlock_t *lock)
+{
+    atomic_flag_clear(lock);
+}
