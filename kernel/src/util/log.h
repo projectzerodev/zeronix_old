7f5f27f9109bf52cb205a71e19ba592f95adda1b
Viktor Popp <viktorpopp.dev@gmail.com>
2025-10-24 14:38:08 +0200

    Improved logging

--- kernel/src/util/log.h
+++ kernel/src/util/log.h
@@ -8,6 +8,7 @@
 #define LOG_COLOR_ERROR "\033[91m"
 #define LOG_COLOR_RESET "\033[0m"
 
+#ifdef VERBOSE_LOGGING
 #define log_debug(fmt, ...)                                                                        \
     kprintf(LOG_COLOR_DEBUG "debug @ %s(): " LOG_COLOR_RESET fmt "\n", __func__, ##__VA_ARGS__)
 #define log_info(fmt, ...)                                                                         \
@@ -16,3 +17,11 @@
     kprintf(LOG_COLOR_WARN " warn @ %s(): " LOG_COLOR_RESET fmt "\n", __func__, ##__VA_ARGS__)
 #define log_error(fmt, ...)                                                                        \
     kprintf(LOG_COLOR_ERROR "error @ %s(): " LOG_COLOR_RESET fmt "\n", __func__, ##__VA_ARGS__)
+#else
+#define log_debug(fmt, ...)                                                                        \
+    kprintf(LOG_COLOR_DEBUG "DEBUG " LOG_COLOR_RESET fmt "\n", ##__VA_ARGS__)
+#define log_info(fmt, ...) kprintf(LOG_COLOR_INFO "INFO  " LOG_COLOR_RESET fmt "\n", ##__VA_ARGS__)
+#define log_warn(fmt, ...) kprintf(LOG_COLOR_WARN "WARN  " LOG_COLOR_RESET fmt "\n", ##__VA_ARGS__)
+#define log_error(fmt, ...)                                                                        \
+    kprintf(LOG_COLOR_ERROR "ERROR " LOG_COLOR_RESET fmt "\n", ##__VA_ARGS__)
+#endif /* VERBOSE_LOGGING */
